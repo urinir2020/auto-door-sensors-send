014792b3aaaaac7895550dcbf2c84cf9bcaec609
urinir2020 <112821482+urinir2020@users.noreply.github.com>
2022-10-26 18:13:18 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-SuperBit.Servo2(SuperBit.enServo.S1, 110)
-radio.setGroup(100)
-basic.forever(function () {
-    if (receivedNumber == 0) {
-        SuperBit.Servo2(SuperBit.enServo.S1, 110)
-    } else {
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
         SuperBit.Servo2(SuperBit.enServo.S1, 220)
+        basic.showArrow(ArrowNames.West)
+    } else {
+        SuperBit.Servo2(SuperBit.enServo.S1, 110)
+        basic.showArrow(ArrowNames.East)
     }
 })
+SuperBit.Servo2(SuperBit.enServo.S1, 110)
+radio.setGroup(100)
